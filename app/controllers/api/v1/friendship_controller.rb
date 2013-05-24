c08290021bfb457c76cfe6d324b4889174a7413f
pedrocarmona <pcarmona1990@gmail.com>
2013-05-24 06:43:03 +0100

    change in controller

--- app/controllers/api/v1/friendship_controller.rb
+++ app/controllers/api/v1/friendship_controller.rb
@@ -3,9 +3,13 @@ class Api::V1::FriendshipController < ApplicationController
 
   def create
     user=User.find_by_facebook_id(params[:friendship][:facebook_id])
-    friend=User.find_by_facebook_id(params[:friendship][:friend_facebook_id])
-    Friendship.create(:user_id => friend.id, :friend_id=>user.id)
-    respond_with Friendship.create(:user_id => user.id, :friend_id=>friend.id)
+    
+    if friend=User.find_by_facebook_id(params[:friendship][:friend_facebook_id])
+      Friendship.create(:user_id => friend.id, :friend_id=>user.id)
+      respond_with Friendship.create(:user_id => user.id, :friend_id=>friend.id)
+    else
+      respond_with render :json => {:msg => "error: no user with that facebook"}
+    end
   end
   
 end
